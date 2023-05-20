aeeb5002c70f6153c5014a242bdd748a947eecc3
Mustansir Bohari <mustansirzain2@gmail.com>
2023-05-20 19:42:58 +0530

    Update run_upload_video.py

--- edictai/app/run_upload_video.py
+++ edictai/app/run_upload_video.py
@@ -5,6 +5,8 @@ def yt_upload_video(filename, title, description):
     # topic = ""
     # keywords = get_top_keywords(topic)
     new_description = description.replace('"', '')
-    os.system(f''' python edictai/app/upload_video.py --file="C:/Users/musta/OneDrive/Desktop/edictAI/edictAI/edict_ai/videos/{filename}" --title="{title}" --description="{new_description}" --keywords="Edict.AI" --category="22" --privacyStatus="unlisted" ''')
+    working_directory = os.getcwd().replace('\\', '/')
+    print(working_directory)
+    os.system(f''' python edictai/app/upload_video.py --file="{working_directory}/videos/{filename}" --title="{title}" --description="{new_description}" --keywords="Edict.AI" --category="22" --privacyStatus="unlisted" ''')
 
 # upload_video("news_edicted_7.mp4")
