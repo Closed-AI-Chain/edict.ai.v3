132d0edb72140b4ba73824fb3fa3a10a55cea9ee
B-Mustansir <108982582+B-Mustansir@users.noreply.github.com>
2023-05-21 15:16:17 +0530

    Update run_upload_video.py

--- edictai/app/run_upload_video.py
+++ edictai/app/run_upload_video.py
@@ -7,6 +7,6 @@ def yt_upload_video(filename, title, description):
     new_description = description.replace('"', '')
     working_directory = os.getcwd().replace('\\', '/')
     print(working_directory)
-    os.system(f''' python edictai/app/upload_video.py --file="{working_directory}/videos/{filename}" --title="{title}" --description="{new_description}" --keywords="Edict.AI" --category="22" --privacyStatus="unlisted" ''')
+    os.system(f''' python3 edictai/app/upload_video.py --file="{working_directory}/videos/{filename}" --title="{title}" --description="{new_description}" --keywords="Edict.AI" --category="22" --privacyStatus="unlisted" ''')
 
 # upload_video("news_edicted_7.mp4")
