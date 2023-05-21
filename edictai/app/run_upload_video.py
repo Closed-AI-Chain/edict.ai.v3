fa0383e059b777050732f1d8824ef9d47930a299
B-Mustansir <108982582+B-Mustansir@users.noreply.github.com>
2023-05-21 15:19:03 +0530

    Update run_upload_video.py

--- edictai/app/run_upload_video.py
+++ edictai/app/run_upload_video.py
@@ -9,4 +9,4 @@ def yt_upload_video(filename, title, description):
     print(working_directory)
     os.system(f''' python3 edictai/app/upload_video.py --file="{working_directory}/videos/{filename}" --title="{title}" --description="{new_description}" --keywords="Edict.AI" --category="22" --privacyStatus="unlisted" ''')
 
-# upload_video("news_edicted_7.mp4")
+upload_video("news_edicted_7.mp4")
